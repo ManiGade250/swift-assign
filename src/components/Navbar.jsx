ef4198f98deb5836062c75b6aa1e0573d1044d53
ManiGade250 <manigopalgade58@gmail.com>
2025-07-08 12:42:52 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,9 +4,15 @@ import './Navbar.css';
 function Navbar() {
   return (
     <nav className="navbar">
-      <div className="navbar-left">SWIFT</div>
-      <div className="navbar-right">Ervin Howell</div>
-    </nav>
+    <div className="logo-section">
+      <div className="logo-icon">S</div>
+      <span className="logo-text">WIFT</span>
+    </div>
+    <div className="profile-section">
+      <div className="avatar">EH</div>
+      <span className="username">Ervin Howell</span>
+    </div>
+  </nav>
   );
 }
 
